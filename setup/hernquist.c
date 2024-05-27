42b799be72f0be304b2e97442e01d72a16f3f55f
DavidBamba <dbambague@unal.edu.co>
2024-05-27 00:49:12 -0230

    herquinst model updated, no final version

--- setup/hernquist.c
+++ setup/hernquist.c
@@ -7,29 +7,41 @@
 
 const double TPI        = 2*M_PI;
 const double sqrt2      = sqrt(2.);
-const double scaleftr   = 16.0/(3.0*M_PI);
-const double invsclftr  = 1./scaleftr;
-const double sqrtscldrt = sqrt(scaleftr);
 
-void spher2cartes(double * Vec, double r, double sclftr) {
-    double X1   = RAND();
-    double X2   = RAND();
-    double aux1 = (1.-2.*X1)*r;
-    double aux2 = sqrt(r*r - aux1*aux1)*sclftr;
-    Vec[0]  = aux2 * cos(TPI*X2);
-    Vec[1]  = aux2 * sin(TPI*X2);
-    Vec[2]  = aux1*sclftr;
-}
 
-double g(double x) {
-    double x2 = x*x;
-    return x2*pow(1-x2, 3.5);
-}
-
+//---------------------------------------------------------------------- //
+// Generate a random number in [min, max] with uniform distribution.     //
+//---------------------------------------------------------------------- //
 double rndm(double min, double max) {
     return min + (max-min)*RAND();
 }
 
+//---------------------------------------------------------------------- //
+// Assign a vector with magnitude 'r' a random direction.                //
+//---------------------------------------------------------------------- //
+void spher2cartes(double * Vec, double r) {
+    double X1   = RAND();
+    double X2   = RAND();
+    double aux1 = (1.-2.*X1)*r;
+    double aux2 = sqrt(r*r - aux1*aux1);
+    Vec[0]  = aux2 * cos(TPI*X2);
+    Vec[1]  = aux2 * sin(TPI*X2);
+    Vec[2]  = aux1;
+}
+
+//---------------------------------------------------------------------- //
+// Probability distribution of velocity.                                 //
+//---------------------------------------------------------------------- //
+double g(double x) {
+    double x2 = x*x;
+    double E = x2*(1-x2);
+    double sqrE = sqrt(-E);
+    return 1.0 / pow(1+E, 5.0/2.0) * (3.0*asin(sqrE) + sqrE*sqrt(1+E) * (1.0+2.0*E)*(8.0*E*E + 8.0*E-3.0));
+}
+
+//---------------------------------------------------------------------- //
+// Convert to the CoM reference frame                                    //
+//---------------------------------------------------------------------- //
 void frm2com(double *Pos, double *Vel, double *Mass, const int N) {
     double x_com    = 0.0;
     double y_com    = 0.0;
@@ -56,61 +68,48 @@ void frm2com(double *Pos, double *Vel, double *Mass, const int N) {
     }
 }
 
-void adjust_units(double *Pos, double*Vel, double *Mass, const int N) {
-    double epot = 0.0;
-    double ekin = 0.0;
-    double xrel = 0.0;
-    double yrel = 0.0;
-    double zrel = 0.0;
-    int ii, jj;
-    for (ii=0; ii<N; ii++) {
-        ekin += Mass[ii]*(Vel[3*ii]*Vel[3*ii] + Vel[3*ii+1]*Vel[3*ii+1] + Vel[3*ii+2]*Vel[3*ii+2]);
-        for (jj = ii+1; jj<N; jj++) {
-            xrel =  Pos[3*ii]-Pos[3*jj];
-            yrel =  Pos[3*ii+1]-Pos[3*jj+1];
-            zrel =  Pos[3*ii+2]-Pos[3*jj+2];
-            epot += -Mass[jj]*Mass[ii] / sqrt(xrel*xrel+yrel*yrel+zrel*zrel);
-        }
-    }
-    double alpha = -2*epot;
-    double beta  = 0.5/sqrt(ekin);
-    for (ii=0; ii<N; ii++) {
-        Pos[3*ii]   *= alpha;
-        Pos[3*ii+1] *= alpha;
-        Pos[3*ii+2] *= alpha;
-        Vel[3*ii]   *= beta;
-        Vel[3*ii+1] *= beta;
-        Vel[3*ii+2] *= beta;
-    }
-}
-
-void hernquist_dist(double *Pos, double *Vel, double *Mass, const int N) {
-    double X1;
-    double sqrt_cum_mass, r, Ve;
+//---------------------------------------------------------------------- //
+// Generate a Hernquist sphere of mass M and parametrized radius R.        //
+// rmax=5R, which is 94% of the total mass for the Hernquist distribution. //
+//---------------------------------------------------------------------- //
+void hernquist_dist(double *Pos, double *Vel, double *Mass, const int N, const double R, const double M, const double seed) {
+    srand(seed);
+    double X1, X2, X3;
+    double r, Ve;
     double m = 1.0/N;
+    double dm = m*M;
     double cum_mass_min = 0.0;
     double cum_mass_max = m;
     int ii;
     for (ii = 0; ii < N; ii++) {
-        Mass[ii] = m;
+        Mass[ii] = dm;
         // Position
-        sqrt_cum_mass = sqrt(rndm(cum_mass_min, cum_mass_max));
-        cum_mass_min = cum_mass_max;
-        cum_mass_max += m;
 
-        r = sqrt_cum_mass / (1 - sqrt_cum_mass); 
-        spher2cartes(Pos + 3*ii, r, invsclftr);
+        do {
+            do {
+                X1 = rndm(cum_mass_min, cum_mass_max);
+                cum_mass_min = cum_mass_max;
+                cum_mass_max += m;
+                X1 = RAND();
+            } while (X1 < 1.0e-10);
+            r = sqrt(X1)/(1-sqrt(X1));
+        } while (r > 5);
+
+        r *=R;
+        spher2cartes(Pos + 3*ii, r);
 
         // Velocity
-        X1   = RAND();
-        while (X1 >= 0.9){
-            X1 = RAND();
-        }; 
+        X2   = RAND();
+        X3   = RAND();
+        while (0.1*X3 < g(X2)) {
+            X2 = RAND();
+            X3 = RAND();
+        } 
 
-        // Calculamos la velocidad de escape proporcional para la partícula a una distancia r
-        Ve = (sqrt2 / sqrt(r + 1)) * X1;
-        spher2cartes(Vel + 3*ii, Ve, sqrtscldrt);
+        Ve = (sqrt2*sqrt(M/R) / sqrt(1.0 + r)) * X1;
+        spher2cartes(Vel + 3*ii, Ve);
     }
+
     frm2com(Pos, Vel, Mass, N);
-    adjust_units(Pos, Vel, Mass, N); 
+
 }
--- setup/hernquist.h
+++ setup/hernquist.h
@@ -1,12 +1,10 @@
 #ifndef HERNQUIST_H_
 #define HERNQUIST_H_
 
+double rndm(double, double);
 void spher2cartes(double *, double, double);
 double g(double);
-double rndm(double, double);
 void frm2com(double *, double *, double *, const int);
-void adjust_units(double *, double*, double *, const int);
-void plummer_dist(double *, double *, double *, const int);
-void hernquist_dist(double *, double *, double *, const int);
+void hernquist_dist(double *, double *, double *, const int, const double, const double, const double);
 
 #endif // HERNQUIST_H_
