38fca86aae95b4f37a3eece4662df26e914767b4
DavidBamba <dbambague@unal.edu.co>
2024-05-29 12:13:53 -0230

    plot of both xy xz planes

--- plot/Animation2D.py
+++ plot/Animation2D.py
@@ -1,4 +1,4 @@
-from numpy import loadtxt, array, fromfile, float64, array
+from numpy import loadtxt, array, fromfile, float64
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 plt.style.use('dark_background')
@@ -29,38 +29,55 @@ N = int(N)
 jump = int(jump)
 steps = int(steps)//jump
 
-#plot
-xi = 0
-yi = 1
+# Data for xy
+xi1 = 0
+yi1 = 1
+initial_x1, initial_y1 = read_data("../Data/Ev_0", N, [xi1, yi1])
 
-# Extract the initial positions of the particles
-initial_x, initial_y = read_data("../Data/Ev_0", N, [xi,yi])
+# Data for xz
+xi2 = 0
+yi2 = 2
+initial_x2, initial_y2 = read_data("../Data/Ev_0", N, [xi2, yi2])
 
-def update_scatter(num, scatter, message):
-    message.set_text(f" t = {(num+1)*jump*dt: .2f} yr")
-    x, y = read_data("../Data/Ev_"+str(num), N, [xi,yi])
-    scatter._offsets = array([x, y]).transpose()
-    return scatter, message
+def update_scatter(num, scatter1, message1, scatter2, message2):
+    message1.set_text(f" t = {(num+1)*jump*dt: .2f} yr")
+    x1, y1 = read_data("../Data/Ev_"+str(num), N, [xi1, yi1])
+    scatter1.set_offsets(array([x1, y1]).transpose())
+    
+    x2, y2 = read_data("../Data/Ev_"+str(num), N, [xi2, yi2])
+    scatter2.set_offsets(array([x2, y2]).transpose())
+    
+    return scatter1, message1, scatter2, message2
 
-# Attaching 3D axis to the figure
-fig = plt.figure()
-ax = fig.add_subplot(111)
+# Creating the figure with two subplots
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 6))
 
-message = ax.text(0.0, 0.9, "", transform=ax.transAxes)
-
-# Creating a scatter plot for N bodies
-scatter = ax.scatter(initial_x, initial_y, c='white', marker='.', s=1)
+# Plot 1
+message1 = ax1.text(0.0, 0.9, "", transform=ax1.transAxes)
+scatter1 = ax1.scatter(initial_x1, initial_y1, c='white', marker='.', s=1)
 boundaries = 7.0 * R
 
-# Setting the axes properties
 if boundaries:
-    ax.set_xlim([-boundaries, boundaries])
-    ax.set_ylim([-boundaries, boundaries])
-    ax.set_xlabel('x [au]')
-    ax.set_ylabel('y [au]')
+    ax1.set_xlim([-boundaries, boundaries])
+    ax1.set_ylim([-boundaries, boundaries])
+    ax1.set_xlabel('x [au]')
+    ax1.set_ylabel('y [au]')
+    ax1.set_title('X-Y Plane')
+
+# Plot 2
+message2 = ax2.text(0.0, 0.9, "", transform=ax2.transAxes)
+scatter2 = ax2.scatter(initial_x2, initial_y2, c='white', marker='.', s=1)
+
+if boundaries:
+    ax2.set_xlim([-boundaries, boundaries])
+    ax2.set_ylim([-boundaries, boundaries])
+    ax2.set_xlabel('x [au]')
+    ax2.set_ylabel('y [au]')
+    ax2.set_title('X-Z Plane')
 
 # Creating the Animation object
-scatter_ani = animation.FuncAnimation(fig, update_scatter, frames=steps, fargs=(scatter, message), interval=1, blit=True, repeat=False)
+scatter_ani = animation.FuncAnimation(fig, update_scatter, frames=steps, fargs=(scatter1, message1, scatter2, message2), interval=1, blit=True, repeat=False)
 
 scatter_ani.save('../Data/Evolution2D.gif', writer='pillow', fps=10)
+plt.tight_layout()
 plt.show()
