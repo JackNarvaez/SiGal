5e03f51db67a518f3cb5625a03ea6ce56dd8192e
DavidBamba <dbambague@unal.edu.co>
2024-06-25 17:02:53 -0230

    corrections to disk distribution

--- setup/Kuzmin.h
+++ setup/Kuzmin.h
@@ -7,6 +7,7 @@ void frm2com(double *, double *, double *, const int);
 void kuzmin_disk(double *, double *, double *, const int, const double, const double, const double);
 double rand_normal(double, double);
 double epicyclic_frequency(double);
+double sample_kuzmin_radius(double );
 double radial_velocity_dispersion( double);
 double tangential_velocity_dispersion( double);
 double vertical_velocity_dispersion( double);
