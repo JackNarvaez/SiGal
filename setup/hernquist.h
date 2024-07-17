5fabdf4da9389f53f3d36882b109ea8b70531b31
DavidBamba <dbambague@unal.edu.co>
2024-07-17 17:07:09 -0230

    hernquist distribution corrections

--- setup/hernquist.h
+++ setup/hernquist.h
@@ -3,6 +3,7 @@
 
 double rndm(double, double);
 void spher2cartes(double *, double, double);
+double hernquist_density(double , double , double );
 double g(double);
 void frm2com(double *, double *, double *, const int);
 void hernquist_dist(double *, double *, double *, const int, const double, const double, const double);
