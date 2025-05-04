867177c1626284b12807c04fc530c881a8c5ad01
Jacksen Narvaez <janarvaez@unal.edu.co>
2025-05-04 20:16:18 -0230

    Define constants

---
+++ setup/constants.c
@@ -0,0 +1,5 @@
+#include <math.h>
+#include "constants.h"
+
+const double TPI        = 2*M_PI;
+const double SQRT2      = sqrt(2.);
---
+++ setup/constants.h
@@ -0,0 +1,7 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+extern const double SQRT2;
+extern const double TPI;
+
+#endif
